0cdefc54be3cff5c97fa50157ff57893a8b98d8f
NguyenNgocDuc <1512127@student.hcmus.edu.vn>
2018-11-13 21:28:48 +0700

    (+) Thêm các thông tin về Họ tên, MSSV, Firebase static hosting, Link Git trong LICENSE

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ListPosts from './Containers/ListPosts';
+import registerServiceWorker from './registerServiceWorker';
+import { applyMiddleware, createStore } from 'redux';
+import thunk from 'redux-thunk';
+import reducers from './Reducers/index';
+import { Provider } from 'react-redux';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Login from './Containers/Login';
+import CreateAccount from './Containers/CreateAccount';
+import LoadingComponent from './Containers/LoadingComponent';
+import AuthenticatedComponent from './Containers/AuthenticatedComponent';
+import PostDetail from './Containers/PostDetail';
+const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <BrowserRouter>
+      <LoadingComponent>
+        <Switch>
+          <Route path="/CreateAccount" component={CreateAccount}/>
+          <Route path="/Login" component={Login}/>
+          <AuthenticatedComponent>
+            <Route path="/:id" component={PostDetail}/>
+            <Route exact path="/" component={ListPosts}/>
+          </AuthenticatedComponent>
+        </Switch>
+      </LoadingComponent>
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root'));
+registerServiceWorker();
